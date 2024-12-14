3054af5750d528a82e153aa7bac22eb4f6203f6d
Ansh Sood <anshsood307@gmail.com>
2024-12-14 10:36:49 +0530

    making templates

--- work_site/work_site/settings.py
+++ work_site/work_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_site',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS=[
+    BASE_DIR/ 'static',
+
+]
